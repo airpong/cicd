ff6e1f3153eaeaf7d99346dcf73c18f2dda8f520
하창언 <changun.Ha@lgcns.com>
2022-05-05 21:16:11 +0900

    init

---
+++ src/main/java/com/example/demo/Member.java
@@ -0,0 +1,24 @@
+// package com.example.demo;
+
+// import javax.persistence.Column;
+// import javax.persistence.Entity;
+// import javax.persistence.GeneratedValue;
+// import javax.persistence.GenerationType;
+// import javax.persistence.Id;
+
+// import lombok.AllArgsConstructor;
+// import lombok.Builder;
+// import lombok.NoArgsConstructor;
+
+// @Entity
+// @NoArgsConstructor
+// @AllArgsConstructor
+// @Builder
+// public class Member {
+    
+//     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     @Column(name = "MEMBER_ID")
+//     private Long id;
+
+//     private String name;
+// }
